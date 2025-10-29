5ad297224674e5b1151c69917e3e2bd9ffafca91
Rinat Rafikov <rafikov.rinat.m@gmail.com>
2025-10-29 20:51:42 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.github.rrin.vulyk"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/com/github/rrin/vulyk/VulykApplication.java
@@ -0,0 +1,11 @@
+package com.github.rrin.vulyk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VulykApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VulykApplication.class, args);
+    }
+}
