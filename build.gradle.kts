7e0d050681eb6de5a1222a5bf22af65eba25b6df
Rinat Rafikov <rafikov.rinat.m@gmail.com>
2025-11-04 20:49:07 +0200

    add JWT security configuration and update database properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
 
+    // Security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("com.auth0:java-jwt:4.4.0")
+
     // Database
     runtimeOnly("org.postgresql:postgresql")
 
--- src/main/java/com/github/rrin/vulyk/VulykApplication.java
+++ src/main/java/com/github/rrin/vulyk/VulykApplication.java
@@ -1,9 +1,12 @@
 package com.github.rrin.vulyk;
 
+import com.github.rrin.vulyk.security.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({JwtProperties.class})
 public class VulykApplication {
     public static void main(String[] args) {
         SpringApplication.run(VulykApplication.class, args);
